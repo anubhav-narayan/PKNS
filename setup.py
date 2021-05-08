5a722a73bb6c536a5aaae39016fde760dc29de21
Anubhav Mattoo <anubhavmattoo@outlook.com>
2021-05-08 15:50:16 +0530

    Major Core API Changes with Renaming and Removal of internal Peergroup variable

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup
 
+
 setup(
-    name='PKNS',
-    version='0.2.0-devel',
-    
+    name='pkns',
+    version='0.4.0',
+    decription='PKNS Framework and CLI',
+    author='Anubhav Mattoo',
+    author_email='anubhavmattoo@outlook.com',
+    packages=['pkns'],
+    scripts=['pkns_cli'],
+    install_requires=[
+        'daemonocle',
+        'sqlitedict',
+        'pycrptodome>=3.7'
+    ],
+    license='AGPLv3',
+    long_description=open('./LICENSE', 'r').read()
 )
